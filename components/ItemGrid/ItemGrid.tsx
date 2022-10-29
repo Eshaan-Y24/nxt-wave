1a0269a9140c26dab761d9a6d619c159bab511e0
Eshaan Yadav <eshaanyadav24@gmail.com>
2022-10-29 17:58:23 +0530

    added tilt highlight to cards

--- components/ItemGrid/ItemGrid.tsx
+++ components/ItemGrid/ItemGrid.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { homepage } from "../../api/homepage";
-import { CustomImage } from "../CustomImage/CustomImage";
-import { Loader } from "../Loader/Loader";
+import { Card } from "../Card/Card";
 import styles from "./ItemGrid.module.scss";
 
 interface ItemGridProps {
@@ -61,44 +60,6 @@ export const ItemGrid: React.FC<ItemGridProps> = ({
       ) : (
         <span>No matches found!</span>
       )}
-    </div>
-  );
-};
-
-interface CardProps {
-  title: string;
-  icon_url: string;
-  link: string;
-  description: string;
-  category: string;
-  tag: string;
-  id: string;
-}
-const Card: React.FC<CardProps> = ({
-  title,
-  icon_url,
-  link,
-  description,
-  category,
-  tag,
-  id,
-}) => {
-  return (
-    <div className={styles.card}>
-      <div className={styles.cardTitleWrapper}>
-        <div className={styles.image}>
-          <CustomImage src={icon_url} alt="" />
-        </div>
-        <div className={styles.cardLabelWrapper}>
-          <p className={styles.cardTitle}>{title}</p>
-          <p className={styles.cardCategory}>{category}</p>
-        </div>
-      </div>
-      <br />
-      <a href={link} className={styles.cardLink}>
-        {link}
-      </a>
-      <p className={styles.cardDescription}>{description}</p>
     </div>
   );
 };
